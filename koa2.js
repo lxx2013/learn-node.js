24f454ebe9364b9ee958acf58e638f408facfe99
lxx.imac <lxx2013@mail.ustc.edu.cn>
2018-08-13 16:02:53 +0800

    test koa-router wechat" ; ;

--- koa2.js
+++ koa2.js
@@ -1,20 +1,38 @@
 const Koa = require('koa')
 const app = new Koa()
+const fs = require('fs')
+const Router = require('koa-router')
+const serve = require('koa-static')
 
-const asyncIO = () => {
-    return new Promise(resolve => setTimeout(resolve, 500))
-}
-
-const mid = () => async (ctx, next) => {
-    ctx.body = 'mark'
-    await next()
-    ctx.body = ctx.body + ' done'
-}
-app.use(mid())
-app.use(async (ctx, next) => {
-    await asyncIO()
-    ctx.body += ' saved'
-    await next()
+var router = new Router()
+router.get('/', ctx => {
+    console.log(ctx.request)
+    console.log(`path${ctx.request.path}`)
+    if (ctx.request.accepts('html')) {
+        ctx.response.type = 'text/html'
+        var t = fs.readFileSync('./images/touma2.jpg')
+        ctx.response.body = "<img src = '/images/touma2.jpg'>"
+    }
 })
-
+router.get('/wechat-hear', (ctx) => {
+  const token = "imooctest"
+  console.log(ctx.query)
+  const {
+    signature,
+    nonce,
+    timestamp,
+    echostr
+  } = ctx.query
+  const str = [token, timestamp, nonce].sort().join('')
+  const sha = str//sha1(str)
+  console.log(sha)
+  if (sha === signature) {
+    ctx.body = echostr
+  } else {
+    ctx.body = 'Failed'
+  }
+})
+app.use(router.routes())
+app.use(router.allowedMethods())
+app.use(serve(__dirname))
 app.listen(3000)
